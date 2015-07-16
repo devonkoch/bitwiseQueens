32ba364aa0ea896579b24c8d69b7074fa59d5726
Devon Koch <devon.r.koch@gmail.com>
2015-07-15 23:00:54 -0700

    final implementation with underscore

--- app.js
+++ app.js
@@ -1,66 +1,52 @@
 var bitwiseNQueens = function(n) {
-  // create options array based on n
-  // if n = 4
-  // teaching bitwise operator
-  var rowOption = 1;
+
+  var firstOption = 1;
   var rowOptions = [];
   for(var i = 0; i < n; i++) {
-    rowOptions.push(rowOption << i);
+    rowOptions.push(firstOption << i);
   }
 
-  console.log('row options: ' + rowOptions) // [1,2,4,8]
+  var solutionsCounter = 0;
 
-  // initialize counter variable to 0
-  var solutionCount = 0;
-  console.log('solution count: ' + solutionCount);
-
-  // recursive function expression
-  var searchForSolutions = function(board) {
-    // base case: board.length = n
-    // console.log('board length: ' + board.length);
+  var findSolutions = function(board) {
+    // base case
     if(board.length === n) {
-      // add 1 to solutionCount and break recursion
-      solutionCount++;
-      return;
-      // console.log('solution count: ' + solutionCount);
+      return solutionsCounter++;
     }
-    /* initial edge case: empty board */
-    if(board.length === 0){
-      // console.log('base case passing');
-      for (var i = 0; i < rowOptions.length; i++) {
-        var initialBoard = [rowOptions[i]];
-        console.log('initial board: ' + initialBoard)
-        searchForSolutions(initialBoard);
-      }
+
+    // initializer
+    if(board.length === 0) {
+      _.each(rowOptions, function(option){
+        findSolutions([option])
+      });
     }
-    // console.log(board);
-    /* looping through potential next row e.g. [1,2,4,8] */ 
-    for (var j = 0; j < rowOptions.length; j++) {
-      /* testing against current board e.g. n = 1 [1], [2], [4], [8] */
-      // [8,1]
-      var boardLen = board.length; // 1
-      
-      for (var k = boardLen - 1; k >= 0; k--) {
-        if(rowOptions[j] === board[k] ||
-           rowOptions[j] === board[k] >> boardLen - k ||
-           rowOptions[j] === board[k] << boardLen - k) {
-             return; 
+
+    // recursion
+    if(board.length > 0){
+      _.each(rowOptions, function(potentialRow){
+
+        var failures = 0;
+
+        _.each(board.reverse(), function(rowToCompare, i){
+          if(potentialRow === rowToCompare ||
+             potentialRow === (rowToCompare >> (board.length - i)) ||
+             potentialRow === (rowToCompare << (board.length - i))) {
+              failures++;
+          }
+        });
+
+        if(!failures){
+          var confirmedRow = potentialRow;
+          var newBoard = board.slice();
+          newBoard.push(confirmedRow);
+          findSolutions(newBoard);
         }
-      }
-      board.push(rowOptions[j]);
-      // console.log(board);
-      searchForSolutions(board);
+      });
     }
   };
-  
-  // run recursive function, takes empty board
-  searchForSolutions([]);
-  
-  // return counter;
-  return solutionCount;
+
+  findSolutions([]);
+
+  return 'A board with ' + n + ' queens has ' + solutionsCounter + ' solutions.'
 
 };
-
-// call bitwiseNQueens
-
-bitwiseNQueens(4);
